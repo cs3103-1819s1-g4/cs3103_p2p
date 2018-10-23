0ae76e4acf19b0e0fd4156e7566ed0dbdd288f59
Marcus <Marcus_cxc@outlook.com>
2018-10-23 22:04:09 +0800

    Add tracker server
    Tracker will now run indefinitely, old tracker (main_server) is not deleted but no longer targeted in cmake. Tracker is able to recieve UDP packet but crash when sending to a closed socket.

---
+++ src/tracker/tracker.cpp
@@ -0,0 +1,159 @@
+//
+// Created by Core on 22/10/2018.
+//
+
+#include "tracker.h"
+
+
+SOCKET listen_sock;
+struct sockaddr_in si_other;
+int slen = sizeof(si_other) , recv_len;
+char buf[PACK_SIZE];
+
+
+
+void tracker::init() {
+
+    struct addrinfo *result = nullptr, hints{};
+    int status;
+
+    ZeroMemory(&result, sizeof (result));
+    ZeroMemory(&hints, sizeof (hints));
+    hints.ai_flags = AI_PASSIVE; // to allow binding
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+
+    status = getaddrinfo(inet_ntoa(server_ip), port, &hints, &result);
+    if (status != 0) {
+        std::cout << "[ERROR]: " << status << " Unable to get address info for Port " << port << ".\n";
+    }
+
+    SOCKET serv_sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (serv_sock == INVALID_SOCKET) {
+        std::cout << "[ERROR]: " << WSAGetLastError() << " Unable to create Socket.\n";
+        freeaddrinfo(result);
+    }
+
+    int on = 1;
+    if( (setsockopt(serv_sock, IPPROTO_IP, IP_HDRINCL, (char *)&on, sizeof(on))) < 0 ) {
+        perror("setsockopt");
+    }
+
+    if (::bind(serv_sock, result->ai_addr, (int)result->ai_addrlen) == SOCKET_ERROR) {
+        std::cout << "[ERROR]: " << WSAGetLastError() << " Unable to bind Socket.\n";
+        freeaddrinfo(result);
+        closesocket(serv_sock);
+    }
+
+    print_main_server((struct sockaddr_in *)result->ai_addr, port);
+    std::cout.flush();
+
+    // We don't need this info any more
+    freeaddrinfo(result);
+    listen_sock = serv_sock;
+
+
+}
+//keep listening for data
+
+void tracker::listen() {
+    while (1) {
+        printf("Waiting for data...");
+        fflush(stdout);
+
+//clear the buffer by filling null, it might have previously received data
+        memset(buf, '\0', PACK_SIZE);
+
+//try to receive some data, this is a blocking call
+        if ((recv_len = recvfrom(listen_sock, buf, PACK_SIZE, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR) {
+            printf("recvfrom() failed with error code : %d", WSAGetLastError());
+            exit(EXIT_FAILURE);
+        }
+
+//print details of the client/peer and the data received
+        printf("Data: %s\n", buf);
+
+        int request = buf[8] - '0';
+
+
+        std::string message(buf);
+        //int pos = message.find("|") + 1;
+        //printf("%s\n", message.substr(pos).c_str());
+
+
+        switch (request) {
+            //Download a file from the swarm. FILENAME value must be filled.
+            case 1:
+                //generateList(message);
+                printf("Generating List %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+                break;
+                //Ask the tracker for the updated list. FILENAME must be filled.
+            case 2:
+                //generateList(message);
+                printf("Generating List %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+                break;
+                //Inform the tracker that a chunk has been successfully downloaded. FILENAME and CHUNK NO must be filled.
+            case 3:
+                //addEntry(message);
+                //printf("Adding Entry from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+                break;
+                //Upload a new file. FILENAME, CHUNK NO and IP ADDRESS must be filled.
+            case 4:
+                //addFile(message);
+                printf("Adding Entry from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+                break;
+                //Exit from swarm. IP ADDRESS must be filled.
+            case 5:
+                //deleteIP(message);
+                printf("Adding Entry from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+                break;
+                //Query the tracker for a list of files available.
+            case 6:
+                //query(message);
+                printf("Adding Entry from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+                break;
+                //Query for a specified file. FILENAME must be filled.
+            case 7:
+                //query(message);
+                printf("Adding Entry from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+                break;
+                //update IP for STUN server
+            case 8:
+                //updateIP(message);
+                break;
+        }
+
+
+        //Sample reply
+        string reply = "Filename.txt||5||127.0.0.1";
+        strcpy(buf,reply.c_str());
+        // TODO: Stop tracker from crashing when attempting to send
+//        if (sendto(listen_sock, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == SOCKET_ERROR)
+//        {
+//            printf("sendto() failed with error code : %d" , WSAGetLastError());
+//        }
+    }
+
+    closesocket(listen_sock);
+    WSACleanup();
+
+}
+string tracker::addEntry(string message){
+    return 0;
+}
+string tracker::addFile(string message){
+    return 0;
+}
+string tracker::query(string message){
+    return 0;
+}
+string tracker::generateList(string message){
+    return 0;
+}
+string tracker::updateIP(string message){
+    return 0;
+}
+string tracker::deleteIP(string message){
+    return 0;
+}
---
+++ src/tracker/tracker.h
@@ -0,0 +1,55 @@
+//
+// Created by Core on 22/10/2018.
+//
+
+#ifndef CS3103_P2P_TRACKER_H
+#define CS3103_P2P_TRACKER_H
+
+#include <WS2tcpip.h>
+#include <winsock.h>
+#include <string>
+#include "../core/core_functions.h"
+
+#pragma comment(lib, "Mswsock.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+#define DEFAULT_TRACKER_PORT "80"
+#define DEFAULT_P2P_SERVER_PORT "6881"
+#define PACK_SIZE 2048  //Max length of buffer
+
+using namespace std;
+
+
+class tracker {
+private:
+    const char *port;
+    IN_ADDR server_ip;
+    WSADATA wsa;
+public:
+
+    tracker(const char *port): port(port) {
+        printf("\nInitialising Winsock...");
+        if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+            printf("Failed. Error Code : %d", WSAGetLastError());
+            exit(EXIT_FAILURE);
+        }
+        printf("Initialised.\n");
+
+
+        get_private_IP(server_ip);
+
+    };
+    void init();
+    string addEntry(string message);
+    string addFile(string message);
+    string query(string message);
+    string generateList(string message);
+    string updateIP(string message);
+    string deleteIP(string message);
+    void listen();
+    void quit();
+
+};
+
+
+#endif //CS3103_P2P_TRACKER_H
